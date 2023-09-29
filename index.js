4261bf6ff71088ca0cb15ac3fcef2a3be32d4811
Guyo <guyo.halake@students.moringaschool.com>
2023-09-29 20:05:41 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+    // send Fetch request to the Game of Thrones API
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((response) => {
+      // Check if the response status is OK (200)
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      // Parse the JSON response and return it
+      return response.json();
+    })
+    .then((data) => {
+      // Call the renderBooks function with the fetched data
+      renderBooks(data);
+    })
+    .catch((error) => {
+      // Handle any errors that occurred during the fetch
+      console.error('Error fetching books:', error);
+    });
 }
-
-function renderBooks(books) {
+  // Implement the renderBooks function to display the data on the web page
+  function renderBooks(books) {
   const main = document.querySelector('main');
-  books.forEach(book => {
+  books.forEach((book) => {
     const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
+    h2.textContent = book.name;
     main.appendChild(h2);
   });
 }
-
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
+  // Call the fetchBooks function when the DOM is loaded
   fetchBooks();
 });
